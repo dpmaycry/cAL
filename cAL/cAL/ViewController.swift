3eaf309738db7ef299a8d83233d6fab5918bfbf7
DP <dp@localhost>
2016-03-11 11:57:45 +0800

    2016-3-11

--- cAL/cAL/ViewController.swift
+++ cAL/cAL/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var isUserInMiddleOfTyping = false
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let num = sender.currentTitle!
+        if isUserInMiddleOfTyping
+        {
+            display.text = num
+        }else
+        {
+            display.text = display.text! + num
+        }
+    }
+    
+    @IBAction func enter(sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
